55299ebe8fd162bab5e32ee875daef16adce26cc
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-04-04 13:37:16 -0500

    THIS time for sure

--- plugin/themery.lua
+++ plugin/themery.lua
@@ -1,5 +1,7 @@
 if vim.g.loaded_themery then
 	return
 end
-require("themery").themery()
+vim.api.nvim_create_user_command('Themery', function ()
+	require("themery").themery()
+end, {})
 vim.g.loaded_themery = true
